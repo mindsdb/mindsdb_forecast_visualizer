62afe236d2fb53524a9053bac7e29b81928dc3e5
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-06-23 15:21:14 -0400

    some small fixes for predictor path

--- mindsdb_forecast_visualizer/core/dispatch.py
+++ mindsdb_forecast_visualizer/core/dispatch.py
@@ -1,13 +1,17 @@
 import os
-
-import pandas as pd
 import mindsdb
-from mindsdb_native import CONFIG, Predictor
+from mindsdb_native import CONFIG, Predictor, __version__, __file__
 
 from mindsdb_forecast_visualizer.core.forecaster import forecast
 
 
 def visualize(predictor_name, df, params=None, subset=None, mode='Native', pred_path=None, rolling=1):
+    if pred_path is None and mode == 'Native':
+            native_path = os.path.dirname(__file__)
+            pred_path = os.path.join(native_path, 'mindsdb_storage', __version__.replace('.', '_'))
+    elif pred_path is None and mode == 'SDK':
+        pred_path = '/storage/predictors'
+
     if mode == 'Native':
         # Path configuration
         mdb_path = mindsdb.root_storage_dir  if not pred_path else pred_path
@@ -17,7 +21,6 @@ def visualize(predictor_name, df, params=None, subset=None, mode='Native', pred_
         mdb_predictor = Predictor(name=predictor_name)
 
         # TODO: fetch params automatically from training params
-
         forecast(mdb_predictor,
                  df,
                  params,
