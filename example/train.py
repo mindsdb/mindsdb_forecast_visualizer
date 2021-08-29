27c94629450bca3407b8c96fbeb9b8647da28c09
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-08-29 13:22:07 -0400

    refactor: annotate train example

--- example/train.py
+++ example/train.py
@@ -1,22 +1,32 @@
 import pandas as pd
-from lightwood.api.high_level import predictor_from_problem, ProblemDefinition
+from lightwood.api.high_level import ProblemDefinition, predictor_from_code, code_from_problem
 
 
 if __name__ == '__main__':
-    p_name = 'arrival_forecast_example'
+    # Load data and define the task
     train_df = pd.read_csv('./arrivals_train.csv')
-
-    pdef = ProblemDefinition.from_dict({'target': 'Traffic',
-                                        'time_aim': 120,
+    pdef = ProblemDefinition.from_dict({'target': 'Traffic',              # column to forecast
+                                        'time_aim': 120,                  # time budget to build a predictor
                                         'nfolds': 10,
                                         'anomaly_detection': True,
                                         'timeseries_settings': {
                                             'use_previous_target': True,
                                             'group_by': ['Country'],
-                                            'nr_predictions': 1,
+                                            'nr_predictions': 1,          # forecast horizon length
                                             'order_by': ['T'],
-                                            'window': 10
+                                            'window': 10                  # qty of previous data to use when predicting
                                         }})
 
-    predictor = predictor_from_problem(train_df, problem_definition=pdef)
+    # name and generate predictor code
+    p_name = 'arrival_forecast_example'
+    predictor_class_code = code_from_problem(train_df, problem_definition=pdef)
+
+    # instantiate and train predictor
+    predictor = predictor_from_code(predictor_class_code)
     predictor.learn(train_df)
+
+    # save predictor and its code
+    predictor.save(f'./{p_name}.pkl')
+    with open(f'./{p_name}.py', 'wb') as fp:
+        fp.write(predictor_class_code.encode('utf-8'))
+
